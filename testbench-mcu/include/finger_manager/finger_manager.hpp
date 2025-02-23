12c1b0fa18e0dd4df82a7abe0b64c37d1b3ec973
Phani Kiran V <phanikiran@u.northwestern.edu>
2025-02-23 17:21:44 -0600

    IC - Finger manager

--- testbench-mcu/include/finger_manager/finger_manager.hpp
+++ testbench-mcu/include/finger_manager/finger_manager.hpp
@@ -1,139 +1,178 @@
 #ifndef __FINGER_MANAGER_H__
 #define __FINGER_MANAGER_H__
 
-#include <vector>
-#include "com_manager/odrive_manager.hpp"
+#include "odrive_manager/odrive_manager.hpp"
+#include <Arduino.h>
 
-/// @brief Basic Communication Manager class for handling multiple CAN buses
+const unsigned long odrive_timeout = 1000; ///< 1 second timeout
+
+/**
+ * @class FingerManager
+ * @brief Manages communication for multiple CAN buses controlling robotic fingers.
+ */
 class FingerManager {
 public:
-    /// @brief Constructor
+    /// @brief Default Constructor
+    FingerManager() = default;
+
+    /**
+     * @brief Parameterized constructor to initialize CAN buses and ODrive managers.
+     * @param canbus0 Reference to CAN bus 0.
+     * @param canbus1 Reference to CAN bus 1.
+     * @param odrive0 Reference to ODriveManager for CAN bus 0.
+     * @param odrive1 Reference to ODriveManager for CAN bus 1.
+     * @param F Callback function for CAN bus receive events.
+     */
     FingerManager(
         FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> &canbus0,
         FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_256> &canbus1,
         ODriveManager<CAN2> &odrive0,
         ODriveManager<CAN3> &odrive1,
-        _MB_ptr F)
-        : canbus0_(canbus0),
-          canbus1_(canbus1),
-          odrive0_(odrive0),
-          odrive1_(odrive1)  
-    {
-        
-    }
+        _MB_ptr F);
 
-    ComManager(
-        
-    {
-        // Initialize the CAN buses
-        canbus0_.begin();
-        canbus0_.setBaudRate(CAN_BAUDRATE);
-        canbus0_.setMaxMB(16);
-        canbus0_.enableFIFO();
-        canbus0_.enableFIFOInterrupt();
-        canbus0_.onReceive(F);
+    /**
+     * @brief Initialize the ODrives and the CAN buses.
+     * @return true if successful, false otherwise.
+     */
+    bool initialize();
 
-        canbus1_.begin();
-        canbus1_.setBaudRate(CAN_BAUDRATE);
-        canbus1_.setMaxMB(16);
-        canbus1_.enableFIFO();
-        canbus1_.enableFIFOInterrupt();
-        canbus1_.onReceive(F);
+    /// @brief Ticks ODrive CAN buses and updates timeouts.
+    void tick();
 
-    }
+    /**
+     * @brief Checks for communication timeout on either ODrive.
+     * @return true if any ODrive is timed out, false otherwise.
+     */
+    bool comms_timeout();
 
-
-    /// \brief Initialize the ODrives and the Teensy CAN, must be called in setup procedure
-    /// \return true if successful
-    /// \return false if unsuccessful
-    bool initialize() {
-        find_odrive(odrive0_);
-        find_odrive(odrive1_);
-
-        odrive0_.startup_odrive_checks();
-        odrive1_.startup_odrive_checks();
-
-        startup_odrive(odrive0_);
-        startup_odrive(odrive1_);
-
-        return true;
-    }
-
-    /// \brief Tick Odrive CAN buses and updates timeouts
-    void tick() {
-
-        pumpEvents(canbus0_);
-        pumpEvents(canbus1_);
-
-        odrive0_.odrive_user_data_.heartbeat_timeout = (millis() - odrive0_.odrive_user_data_.last_heartbeat_time) > ODRIVE_HEARTBEAT_TIMEOUT;
-        odrive1_.odrive_user_data_.heartbeat_timeout = (millis() - odrive1_.odrive_user_data_.last_heartbeat_time) > ODRIVE_HEARTBEAT_TIMEOUT;
-        
-    }
-
-    /// \brief Communication timeout on either of ODrives
-    /// \return true if any are timed out
-    /// \return false if none are timed out
-    bool comms_timeout() {
-        return odrive0_.odrive_user_data_.heartbeat_timeout or 
-               odrive1_.odrive_user_data_.heartbeat_timeout;
-
-    }
-
-    /// \brief Receive hearbeats on odrives
+    /**
+     * @brief Receives heartbeats from ODrives.
+     * @tparam T ODrive type.
+     * @param odrive The ODrive instance to check.
+     * @return true if the ODrive was found, false if timeout occurred.
+     */
     template <typename T>
-    void find_odrive(T odrive)
-    {
-        Serial.println("Waiting for ODrive" + String(odrive.odrive_user_data_.node_id) + "...");
-        while (not odrive.odrive_user_data_.received_heartbeat)
-        {
-            tick();
-            delay(100);
-            Serial.println("Waiting for ODrive" + String(odrive.odrive_user_data_.node_id) + "...");
-        }
-        Serial.println("Found ODrive");
-    }
+    bool find_odrive(T &odrive);
 
-    /// \brief Enable closed loop control on odrive
+    /**
+     * @brief Enables closed-loop control on ODrive.
+     * @tparam T ODrive type.
+     * @param odrive The ODrive instance to start.
+     * @return true if successful, false otherwise.
+     */
     template <typename T>
-    void startup_odrive(T odrive)
-    {
-        Serial.println("Enabling closed loop control...");
-        while (odrive.odrive_user_data_.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL)
-        {
-            delay(1);
-            odrive.odrive_.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
-            
-            // Pump events for 150ms. This delay is needed for two reasons;
-            // 1. If there is an error condition, such as missing DC power, the ODrive might
-            //    briefly attempt to enter CLOSED_LOOP_CONTROL state, so we can't rely
-            //    on the first heartbeat response, so we want to receive at least two
-            //    heartbeats (100ms default interval).
-            // 2. If the bus is congested, the setState command won't get through
-            //    immediately but can be delayed.
-            for (int i = 0; i < 15; ++i)
-            {
-                delay(10);
-                ComManager::tick();
-            }
-        }
-        Serial.println("ODrive running!");
-    }
-
+    bool startup_odrive(T &odrive);
 
 public:
-
-    /// \brief Canline for first odrive
-    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> &canbus0_;
-
-    /// \brief Canline for second odrive
-    FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_256> &canbus1_;
-
-    /// \brief Address of first odrive
-    ODriveManager<CAN2> &odrive0_;
-
-    /// \brief Address of second odrive
-    ODriveManager<CAN3> &odrive1_;
-
+    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> &canbus0_; ///< CAN bus for first ODrive.
+    FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_256> &canbus1_; ///< CAN bus for second ODrive.
+    ODriveManager<CAN2> &odrive0_; ///< Address of first ODrive.
+    ODriveManager<CAN3> &odrive1_; ///< Address of second ODrive.
 };
 
-#endif // __COM_MANAGER_H__
+
+FingerManager::FingerManager(
+    FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> &canbus0,
+    FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_256> &canbus1,
+    ODriveManager<CAN2> &odrive0,
+    ODriveManager<CAN3> &odrive1,
+    _MB_ptr F)
+    : canbus0_(canbus0), canbus1_(canbus1), odrive0_(odrive0), odrive1_(odrive1) {
+    
+    canbus0_.begin();
+    canbus0_.setBaudRate(CAN_BAUDRATE);
+    canbus0_.setMaxMB(16);
+    canbus0_.enableFIFO();
+    canbus0_.enableFIFOInterrupt();
+    canbus0_.onReceive(F);
+
+    canbus1_.begin();
+    canbus1_.setBaudRate(CAN_BAUDRATE);
+    canbus1_.setMaxMB(16);
+    canbus1_.enableFIFO();
+    canbus1_.enableFIFOInterrupt();
+    canbus1_.onReceive(F);
+}
+
+bool FingerManager::initialize() {
+    bool odrive0_found = find_odrive(odrive0_);
+    bool odrive1_found = find_odrive(odrive1_);
+
+    if (!odrive0_found || !odrive1_found) {
+        Serial.println("ERROR: Failed to find one or both ODrives. Initialization aborted.");
+        return false;
+    }
+
+    odrive0_.startup_odrive_checks();
+    odrive1_.startup_odrive_checks();
+
+    bool odrive0_started = startup_odrive(odrive0_);
+    bool odrive1_started = startup_odrive(odrive1_);
+
+    if (!odrive0_started || !odrive1_started) {
+        Serial.println("ERROR: Failed to start one or both ODrives. Initialization aborted.");
+        return false;
+    }
+
+    Serial.println("Initialization successful!");
+    return true;
+}
+
+void FingerManager::tick() {
+    pumpEvents(canbus0_);
+    pumpEvents(canbus1_);
+
+    odrive0_.odrive_user_data_.heartbeat_timeout = (millis() - odrive0_.odrive_user_data_.last_heartbeat_time) > ODRIVE_HEARTBEAT_TIMEOUT;
+    odrive1_.odrive_user_data_.heartbeat_timeout = (millis() - odrive1_.odrive_user_data_.last_heartbeat_time) > ODRIVE_HEARTBEAT_TIMEOUT;
+}
+
+bool FingerManager::comms_timeout() {
+    return odrive0_.odrive_user_data_.heartbeat_timeout || 
+           odrive1_.odrive_user_data_.heartbeat_timeout;
+}
+
+template <typename T>
+bool FingerManager::find_odrive(T &odrive) {
+    Serial.println("Waiting for ODrive" + String(odrive.odrive_user_data_.node_id) + "...");
+
+    unsigned long start_time = millis();
+    while (!odrive.odrive_user_data_.received_heartbeat) {
+        tick();
+        delay(100);
+        Serial.println("Waiting for ODrive" + String(odrive.odrive_user_data_.node_id) + "...");
+
+        if (millis() - start_time > odrive_timeout) {
+            Serial.println("ERROR: Timeout while waiting for ODrive" + String(odrive.odrive_user_data_.node_id) + " heartbeat.");
+            return false;
+        }
+    }
+
+    Serial.println("Found ODrive" + String(odrive.odrive_user_data_.node_id));
+    return true;
+}
+
+template <typename T>
+bool FingerManager::startup_odrive(T &odrive) {
+    Serial.println("Enabling closed loop control...");
+    unsigned long start_time = millis();
+
+    while (odrive.odrive_user_data_.last_heartbeat.Axis_State != ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL) {
+        delay(1);
+        odrive.odrive_.setState(ODriveAxisState::AXIS_STATE_CLOSED_LOOP_CONTROL);
+        
+        for (int i = 0; i < 15; ++i) {
+            delay(10);
+            tick();
+        }
+
+        if (millis() - start_time > odrive_timeout) {
+            Serial.println("ERROR: Timeout while enabling closed loop control for ODrive" + String(odrive.odrive_user_data_.node_id));
+            return false;
+        }
+    }
+
+    Serial.println("ODrive" + String(odrive.odrive_user_data_.node_id) + " running!");
+    return true;
+}
+
+#endif // __FINGER_MANAGER_H__
--- testbench-mcu/src/main.cpp
+++ testbench-mcu/src/main.cpp
@@ -1,8 +1,6 @@
 #include "teensy_can.h"
-#include "com_manager/com_manager.hpp"
-#include "com_manager/odrive_manager.hpp"
-#include "state_manager/state_manager.hpp"
-#include <vector>
+#include "finger_manager/finger_manager.hpp"
+#include "odrive_manager/odrive_manager.hpp"
 
 FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_256> canbus0;
 FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_256> canbus1;
@@ -23,15 +21,13 @@ void onOdriveCanMessage(const CanMsg &msg)
     onReceive(msg, odrive1.odrive_);
 }
 
-ComManager comms_manager{canbus0,
-                         canbus1,
-                         odrive0,
-                         odrive1,
-                         onOdriveCanMessage};
+FingerManager finger_mananger{canbus0,
+                              canbus1,
+                              odrive0,
+                              odrive1,
+                              onOdriveCanMessage};
 
-StateManager state_manager{comms_manager};                         
 
-VirtualTimerGroup timer_group_ms{}, time_group_us{};
 
 void setup() {
     
@@ -47,22 +43,10 @@ void setup() {
 
     Serial.println("Starting CAN setup");
 
-    if (!comms_manager.initialize())
+    if (!finger_mananger.initialize())
     {
         Serial.println("CAN failed to initialize: reset required");
     }
-
-    // Setup deadman switch
-    pinMode(DEADMAN_SWITCH, INPUT);
-
-    // Interrupt setup
-    attachInterrupt(digitalPinToInterrupt(DEADMAN_SWITCH), []() { state_manager.check_deadman(); }, CHANGE);
-
-    timer_group_ms.AddTimer(10, []() { state_manager.update_led(); });
-    timer_group_ms.AddTimer(10, []() { state_manager.change_state(); });
-
-    time_group_us.AddTimer(500, []() { state_manager.execute_state(); });
-
 
     Serial.println("Setup complete");
 
@@ -70,13 +54,6 @@ void setup() {
 
 void loop() {
 
-    comms_manager.tick();
-    timer_group_ms.Tick(millis());
-    time_group_us.Tick(micros());
-
-
-    odrive0.set_position(0.1);
-    odrive1.set_position(0.1);
 
     if (odrive0_user_data.received_feedback) {
         Get_Encoder_Estimates_msg_t feedback = odrive0_user_data.last_feedback;
